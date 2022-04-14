ec746a5706e86a5f0adb48d4c3e84064b993579c
toMilanesi <tomi.milanesi@gmail.com>
2022-04-14 16:03:13 -0300

    basic html and css

---
+++ script.js
@@ -0,0 +1,7 @@
+const imagenes = ["imagen1", "imagen2", "imagen3", "imagen4", "imagen5", "imagen6", "imagen1C", "imagen2C", "imagen3C", "imagen4C", "imagen5C", "imagen6C"];
+
+function randomNum(){
+    return Math.ceil(Math.random()*12);
+}
+
+console.log(randomNum())
